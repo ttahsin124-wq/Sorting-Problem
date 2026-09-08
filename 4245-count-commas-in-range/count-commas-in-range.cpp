f8e1d70bb8d1163be671883beceac3604368f18e
Anisa Tahsin Nisa <ttahsin124@gmail.com>
2026-09-08 21:56:22 +0600

    Time: 3 ms (13.67%) | Memory: 8.6 MB (51.38%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long countCommas(int n) {
+        long long ans = 0;
+        for (int i = 1; i <= n; i++) {
+            int x = i;
+            while (x >= 1000) {
+                ans++;
+                x /= 1000;
+            }
+        }
+        return ans;
+    }
+};
