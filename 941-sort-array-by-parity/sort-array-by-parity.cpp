3a2d5560565ce4423c0984978d5f1f96cb1843f1
Anisa Tahsin Nisa <ttahsin124@gmail.com>
2026-09-13 21:45:12 +0600

    Time: 0 ms (100.00%) | Memory: 21.2 MB (5.95%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cpp
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -4,7 +4,7 @@ public:
     vector<int>even,odd;
     for(int x:nums)
     {
-        if(x%2==0)
+        if(x%2==0)   // for reminder sort by parity means even first then odd!
         {
             even.push_back(x);
         }
